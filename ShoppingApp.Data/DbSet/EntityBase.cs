4ebcd92d36fa7b35cecfa164fc54744921f5110b
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-11 12:00:53 -0600

    All dbset should implement entity base

--- ShoppingApp.Data/DbSet/EntityBase.cs
+++ ShoppingApp.Data/DbSet/EntityBase.cs
@@ -3,6 +3,6 @@ namespace shoppingApp.Data.DbSet
     public class EntityBase
     {
         public Guid Id { get; set; }
-        public DateTime DateCreated { get; set; }
+        public DateTime? DateCreated { get; set; }
     }
 }
