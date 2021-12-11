2590dd51c61f244735fff15f9d27eb626fb9333e
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-11 12:08:54 -0600

    Context changes from dev migrations and REST

---
+++ ShoppingApp.Data/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using shoppingApp.Data.DbSet;
+
+namespace shoppingApp.Data.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions) { }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Business>? Businesses { get; set; }
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Order>? Orders { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-4AVB77M\\TOOENSURE; Initial Catalog=ShoppingAppDb; Integrated Security=True");
+        }
+    }
+}
