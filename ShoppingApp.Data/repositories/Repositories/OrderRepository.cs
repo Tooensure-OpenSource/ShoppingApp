1d3ddcb8fbdf4ef591b8dc69cc9130b7ef74eeac
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-13 03:26:35 -0600

    Repository updates

--- ShoppingApp.Data/repositories/Repositories/OrderRepository.cs
+++ ShoppingApp.Data/repositories/Repositories/OrderRepository.cs
@@ -19,22 +19,5 @@ namespace shoppingApp.Data.repositories.Repositories
         {
             throw new NotImplementedException();
         }
-
-
-        public async Task<IEnumerable<Order>> Get(Guid userId, Guid businessId)
-        {
-            return await _dbSet
-            .Include(x => x.OrderItems)
-            .Include(x => x.Business)
-            .Where(x => x.BuisnessId == businessId)
-            .ToListAsync();
-        }
-
-
-        ///<summary>
-        /// Fact: passing useful information like userId, then the business id, 
-        /// followed by the order Id will in fact return a specfic order.
-        ///</summary>
-
     }
 }
--- ShoppingApp.Data/repositories/Repositories/ProductRepository.cs
+++ ShoppingApp.Data/repositories/Repositories/ProductRepository.cs
@@ -7,15 +7,8 @@ namespace shoppingApp.Data.repositories.Repositories
     public class ProductRepository : GenericRepository<Product>, IProductRepository
     {
         public ProductRepository(DbContext context) : base(context)
-        {
-        }
-
-        public async Task<IEnumerable<Product>> All(Guid businessId)
-        {
-            return await _dbSet
-            .Where(x => x.BuisnessId == businessId)
-            .ToListAsync();
-        }
+        {}
+        
 
         public override async Task<Product> Get(Guid productId)
         {
@@ -32,28 +25,11 @@ namespace shoppingApp.Data.repositories.Repositories
             throw new NotImplementedException();
         }
 
-        public async Task<Product> Get(Guid businessId, Guid ProductId)
+
+        public async Task<bool> Exist(Guid productId)
         {
             return await _dbSet
-            .Where(x => x.BuisnessId == businessId)
-            .FirstAsync(x => x.Id == ProductId);
-        }
-
-        public async Task<IEnumerable<Product>> ProductsForBusiness(Guid userId, Guid businessId)
-        {
-            return await _dbSet
-            .Where(x => x.BuisnessId == businessId)
-            .ToListAsync();
-        }
-
-        public Task<Product> Get(Guid userId, Guid businessId, Guid ProductId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Product>> ProductsForUser(Guid userId)
-        {
-            throw new NotImplementedException();
+            .AnyAsync(x => x.Id == productId);
         }
     }
 }
