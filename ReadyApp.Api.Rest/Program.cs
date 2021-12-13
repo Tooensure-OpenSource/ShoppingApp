b98245832a9996e447b44f2b34f9fbef43a997dd
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-13 03:34:23 -0600

    Programs configuration file

--- ReadyApp.Api.Rest/Program.cs
+++ ReadyApp.Api.Rest/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using shoppingApp.Data.Data;
 using shoppingApp.Data.IConfigeration;
@@ -6,13 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.AddDbContext<AppDbContext>(opt =>
-        opt.UseSqlServer(builder.Configuration.GetConnectionString("ReadyAppDb"))
-        .EnableSensitiveDataLogging()
-        .UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll));
+builder.Services.AddDbContext<AppDbContext>(opt => {
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ReadyAppDb"))
+    .EnableSensitiveDataLogging()
+    .UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll);
+});
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddApiVersioning(opt => {
+    opt.ReportApiVersions = true;
+    // Automatically provide default version
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.DefaultApiVersion = ApiVersion.Default;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -20,7 +28,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseRouting();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -29,8 +37,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
+// app.UseAuthorization();
 
 app.MapControllers();
 
