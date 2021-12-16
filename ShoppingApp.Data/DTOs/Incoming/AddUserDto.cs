9397fddefbe6108e201392a09f3e37432b2d1a90
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-16 06:10:28 -0600

    Add user requires Email

--- ShoppingApp.Data/DTOs/Incoming/AddUserDto.cs
+++ ShoppingApp.Data/DTOs/Incoming/AddUserDto.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using ShoppingApp.Data.DTOs.Incoming;
+
 namespace ShoppingApp.Data.DTOs.UserDto
 {
-    public class AddUserDto
+    public class AddUserDto : UserForManipulationDto
     {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string EmailAddress { get; set; } = string.Empty;
+        [Required,EmailAddress]
+        public override string EmailAddress { get; set; } = string.Empty;
     }
 }
