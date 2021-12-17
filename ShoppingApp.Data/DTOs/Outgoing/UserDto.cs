00aac91454d183c0429cdc9049ed3853f7fbd74f
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-17 03:36:26 -0600

    User dto now reponds with added user

--- ShoppingApp.Data/DTOs/Outgoing/UserDto.cs
+++ ShoppingApp.Data/DTOs/Outgoing/UserDto.cs
@@ -5,6 +5,8 @@ namespace ShoppingApp.Data.DTOs.UserDto
         public Guid Id { get; set; } = Guid.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+
         public string EmailAddress { get; set; } = string.Empty;
         public int Orders { get; set; }
         public int Businesses { get; set; }
