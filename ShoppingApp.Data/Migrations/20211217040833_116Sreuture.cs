54a85799939e22b6061c57e93063476ed37dd5a1
Shawn Bellazan <tooensuremaster@gmail.com>
2021-12-17 02:08:41 -0600

    Migration files

---
+++ ShoppingApp.Data/Migrations/20211217040833_116Sreuture.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingApp.Data.Migrations
+{
+    public partial class _116Sreuture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Products_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "OrderItem");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderItemId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_OrderItemId",
+                table: "Products",
+                column: "OrderItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_OrderItem_OrderItemId",
+                table: "Products",
+                column: "OrderItemId",
+                principalTable: "OrderItem",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_OrderItem_OrderItemId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_OrderItemId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OrderItemId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId",
+                table: "OrderItem",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_ProductId",
+                table: "OrderItem",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Products_ProductId",
+                table: "OrderItem",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ ShoppingApp.Data/Migrations/20211217045516_126Sreuture.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingApp.Data.Migrations
+{
+    public partial class _126Sreuture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderItem_Orders_OrderId",
+                table: "OrderItem");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_OrderItem_OrderItemId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_OrderItemId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderItem_OrderId",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "OrderItemId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "OrderItem");
+
+            migrationBuilder.CreateTable(
+                name: "OrderItemProduct",
+                columns: table => new
+                {
+                    OrderItemsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItemProduct", x => new { x.OrderItemsId, x.ProductsId });
+                    table.ForeignKey(
+                        name: "FK_OrderItemProduct_OrderItem_OrderItemsId",
+                        column: x => x.OrderItemsId,
+                        principalTable: "OrderItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderItemProduct_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderOrderItem",
+                columns: table => new
+                {
+                    OrderItemsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrdersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderOrderItem", x => new { x.OrderItemsId, x.OrdersId });
+                    table.ForeignKey(
+                        name: "FK_OrderOrderItem_OrderItem_OrderItemsId",
+                        column: x => x.OrderItemsId,
+                        principalTable: "OrderItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderOrderItem_Orders_OrdersId",
+                        column: x => x.OrdersId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItemProduct_ProductsId",
+                table: "OrderItemProduct",
+                column: "ProductsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderOrderItem_OrdersId",
+                table: "OrderOrderItem",
+                column: "OrdersId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItemProduct");
+
+            migrationBuilder.DropTable(
+                name: "OrderOrderItem");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderItemId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderId",
+                table: "OrderItem",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_OrderItemId",
+                table: "Products",
+                column: "OrderItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_OrderId",
+                table: "OrderItem",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderItem_Orders_OrderId",
+                table: "OrderItem",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_OrderItem_OrderItemId",
+                table: "Products",
+                column: "OrderItemId",
+                principalTable: "OrderItem",
+                principalColumn: "Id");
+        }
+    }
+}
